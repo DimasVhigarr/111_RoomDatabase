8807f73620bfa0456df0e19b11198b484d5c9356
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-12-05 20:33:42 +0700

    add fun DataSiswa

---
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -0,0 +1,20 @@
+package com.example.roomsiswa.ui.theme.halaman
+
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.roomsiswa.data.Siswa
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {}
+}
