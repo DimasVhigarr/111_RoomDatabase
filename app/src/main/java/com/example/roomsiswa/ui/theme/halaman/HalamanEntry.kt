ed1800bb83c74bd70f1d9e52ec27c86a82e6f058
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-12-05 20:06:55 +0700

    add column

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,14 +1,17 @@
 package com.example.roomsiswa.ui.theme.halaman
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import com.example.roomsiswa.R
 import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
-object DestinasiEntry: DestinasiNavigasi {
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
@@ -18,6 +21,10 @@ object DestinasiEntry: DestinasiNavigasi {
 fun FormInputSiswa(
     detailSiswa: DetailSiswa,
     modifier: Modifier = Modifier,
-    onValueChange: (DetailSiswa) -> Unit  = {},
+    onValueChange: (DetailSiswa) -> Unit = {},
     enabled: Boolean = true
-)
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {}
