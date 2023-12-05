1a2ca7b97e34c06a0ed35603567b2c6f6aabffeb
DimasVhigarr <dimasvhigar2002@gmail.com>
2023-12-05 20:00:47 +0700

    add class aplikasisiswa dan object PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,10 +1,7 @@
 package com.example.roomsiswa.model
 
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.roomsiswa.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -16,6 +13,3 @@ object PenyediaViewModel {
         }
     }
 }
-
-fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
